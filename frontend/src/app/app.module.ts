6a10f0835a1e221d0820c68de37684dd0e610b98
Henrique da Silva Lima <henrikelima.0502@gmail.com>
2020-07-02 11:29:42 -0300

    finish project v0.0.1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,6 +30,8 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -44,7 +46,9 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     RedDirective,
     ForDirective,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
